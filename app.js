211037dde07502b29312b41ffdaf2786101c7108
Dmytro Fediushyn <dimafed116@gmail.com>
2023-06-17 21:44:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+$(".meet").hide();
+
+
+$( ".don" ).on( "click", function( event ) {
+  alert('Донат відправлений успішно')    
+});
+
+$(".inv").on("click" , function(event){
+  
+  $(".meet").show();
+});
+
+
+
+
+
